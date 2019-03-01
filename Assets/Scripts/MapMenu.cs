acdfa6b13f856d1f0f104b371c154d16fa2dae29
femi96 <imef96@gmail.com>
2019-03-01 10:42:46 -0500

    Add map scene

---
+++ Assets/Scripts/MapMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapMenu : MonoBehaviour {
+
+  // Map controls when battles/cutscenes happen
+
+  public void MapStartTestBattle() {
+    SceneManager.LoadScene("TestBattleScene", LoadSceneMode.Single);
+  }
+
+  public void MapStartTestCutscene() {
+    SceneManager.LoadScene("TestBattleScene", LoadSceneMode.Single);
+  }
+
+  public void MapToParty() {
+    SceneManager.LoadScene("TestBattleScene", LoadSceneMode.Single);
+  }
+
+  public void MapSaveQuit() {
+    Party.SaveParty();
+    SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Single);
+  }
+}
